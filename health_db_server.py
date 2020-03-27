56f08852e221d9044a91e492e15ea449a93e1c30
yl642 <yl642@duke.edu>
2020-03-27 17:37:41 -0400

    Created add new patient route

--- health_db_server.py
+++ health_db_server.py
@@ -17,6 +17,33 @@ def init_database():  # test entries, not mandatory
     # Add code to start the logging
 
 
+@app.route("/new_patient", mathods=["POST"])
+def post_new_patient():
+    """
+    Receive the posting json
+    Verify the json contains the correct keys and data  # write this first!
+    If data is bad, reject request with bad status to client
+    If data is good, add patient to database, and return good status to client
+    """
+    in_dict = request.get_json()
+    check_result = verify_new_patient_info(in_dict)
+    if check_result is not True:
+        return check_result, 400
+    add_patient_to_db(in_dict["name"], in_dict["id"], in_dict["age"])
+    return "Patient added", 200
+
+
+def verify_new_patient_info(in_dict):
+    expected_keys = ("name", "id", "age")
+    expected_types = (str, int, int)
+    for idx, key in enumerate(expected_keys):
+        if key not in in_dict.keys():  # not in
+            return "{} key not found".format(key)
+        if type(in_dict[key] is not expected_types[idx]):  # is not
+            return "{} value not correct type".format(key)
+    return True
+
+
 if __name__ == '__main__':
     init_database()
     app.run()
